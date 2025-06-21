7f1f6796f8f5644b785eeb8affb2360404d5d07f
Vidhan2327 <vidhansinghvaish@gmail.com>
2025-06-22 03:03:27 +0530

    Initial commit

---
+++ src/components/Basics/menuApi.js
@@ -0,0 +1,120 @@
+const Menu = [
+  // 🥣 Breakfast
+  {
+    id: 1,
+    image: "images/corn.jpg",
+    name: "corn",
+    category: "Breakfast",
+    price: "10₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in.",
+  },
+  {
+    id: 2,
+    image: "images/paubhaji.jpg",
+    name: "Pav Bhaji",
+    category: "Breakfast",
+    price: "50₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in.",
+  },
+  {
+    id: 3,
+    image: "images/puri.jpg",
+    name: "Puri Bhaji",
+    category: "Breakfast",
+    price: "50₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in.",
+  },
+
+  // 🍛 Lunch
+  {
+    id: 4,
+    image: "../images/chola.jpg",
+    name: "chola",
+    category: "Lunch",
+    price: "60₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in.",
+  },
+  {
+    id: 5,
+    image: "../images/rajmarice.jpg",
+    name: "Rajma Rice",
+    category: "Lunch",
+    price: "80₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in.",
+  },
+
+  // ☕ Evening
+ 
+  {
+    id: 6,
+    image: "images/maggi.jpg",
+    name: "maggi",
+    category: "Evening",
+    price: "20₹",
+    description:
+      "I love Maggi really. Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto.",
+  },
+  {
+    id: 7,
+    image: "../images/pizza.jpg",
+    name: "pizza",
+    category: "Evening",
+    price: "100₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in.",
+  },
+  {
+    id: 8,
+    image: "../images/samosa.jpg",
+    name: "samosa",
+    category: "Evening",
+    price: "20₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in.",
+  },
+  {
+    id: 9,
+    image: "/images/momo.jpg",
+    name: "Momos",
+    category: "Evening",
+    price: "60₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in.",
+  },
+
+  // 🍽️ Dinner
+   {
+    id: 10,
+    image: "images/vegthali.jpg",
+    name: "Veg Thali",
+    category: "Dinner",
+    price: "200₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in.",
+  },
+  {
+    id: 11,
+    image: "../images/nonvegthali.jpg",
+    name: "Non-Veg Thali",
+    category: "Dinner",
+    price: "250₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in.",
+  },
+  {
+    id: 12,
+    image: "../images/sweet.jpg",
+    name: "Sweets",
+    category: "Dinner",
+    price: "100₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in.",
+  },
+];
+
+export default Menu;
