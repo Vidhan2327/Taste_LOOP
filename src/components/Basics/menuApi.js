795369faa1ffd218a628dc2d38fd45643597c9e4
Vidhan2327 <vidhansinghvaish@gmail.com>
2025-06-22 11:02:15 +0530

    Initial commit

--- src/components/Basics/menuApi.js
+++ src/components/Basics/menuApi.js
@@ -115,6 +115,7 @@ const Menu = [
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in.",
   },
+  
 ];
 
 export default Menu;
